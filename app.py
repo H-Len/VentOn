f02b5f8846350bc29e6f1cfb5bf3da276ffe139a
Hannah Melendy <8978475+H-Len@users.noreply.github.com>
2022-07-16 20:22:42 -0700

    hard code for the evening

--- app.py
+++ app.py
@@ -164,5 +164,5 @@ def grateful():
 
 
 if __name__ == '__main__':
-    port=int(os.environ.get('PORT',5000))
-    app.run(debug= True, port = port)
+    # port=int(os.environ.get('PORT', 5000))
+    app.run(debug= True, port = 33507)
