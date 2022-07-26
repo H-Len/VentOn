79438fa023901e73b9dcb9d484027f4fd49f27dd
Hannah Melendy <8978475+H-Len@users.noreply.github.com>
2022-07-25 20:27:28 -0700

    added missing () to datetime.now call

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app.config['SECRET_KEY'] = 'k3n%L$knn(9()wl_-o'
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now())
     title = db.Column(db.Text, nullable=False) 
     content = db.Column(db.Text, nullable=False)
     init_mood = db.Column(db.SmallInteger)
