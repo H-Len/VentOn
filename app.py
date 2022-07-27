f852e65187e3d2b7abc316c0326dbb05717e02bd
Hannah Melendy <8978475+H-Len@users.noreply.github.com>
2022-07-26 19:37:55 -0700

    add try statement to port

--- app.py
+++ app.py
@@ -164,4 +164,8 @@ def grateful():
 
 
 if __name__ == '__main__':
-    app.run(debug= True, port = 5000)
+    try:
+        app.run(debug= True, port = 5000)
+    except:
+        app.run(debug=True, port = 5432)
+    
